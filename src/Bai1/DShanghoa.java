029bf8424cff8e6abddfe5e9fb394296bf443779
TuanNQ2209 <119795117+TuanNQ2209@users.noreply.github.com>
2024-03-22 09:46:39 +0700

    Add files via upload

---
+++ src/Bai1/DShanghoa.java
@@ -0,0 +1,36 @@
+package Bai1;
+
+import java.util.Scanner;
+
+public class DShanghoa {
+    private int soluong;
+    private HANGHOA ds[];
+
+    public DShanghoa() {
+        ds = new HANGHOA[100];
+        soluong = 0;
+    }
+
+    public void themMH(HANGHOA h) {
+        ds[soluong] = h;
+        soluong++;
+    }
+
+    public void Xuat(byte loai) {
+        System.out.println("\nDanh sách hàng hóa:");
+        if (loai == 1) {
+            for (int i = 0; i < soluong; i++) {
+                if (ds[i] instanceof HANGDM) {
+                    HANGDM DienMay = (HANGDM) ds[i];
+                    DienMay.xuat();
+                }
+            }
+        } else if (loai == 3) {
+            for (int i = 0; i < soluong; i++) {
+                if (ds[i] instanceof HANGTP) {
+                    HANGTP ThucPham = (HANGTP) ds[i];
+                    ThucPham.xuat();
+                }
+            }
+        }
+    }}
